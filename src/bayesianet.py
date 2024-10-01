c4ad79184e1b9f75009ede0118d589b9a89ae449
ShihanYang <dr.yangsh@gmail.com>
2024-10-01 22:46:35 +0800

    changed for bayesian network inference using

--- src/bayesianet.py
+++ src/bayesianet.py
@@ -14,6 +14,7 @@
 """
 import math
 import os
+
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # only ERROR messages
 
 import numpy as np
@@ -36,7 +37,7 @@ class Node:
         cpt = np.array(cptable)
         if len(cptable) == 0:
             p = np.random.random()
-            self.cptable = np.array([p, 1-p])
+            self.cptable = np.array([p, 1 - p])
         else:
             self.cptable = cpt
 
@@ -70,10 +71,10 @@ class Node:
                     str_i = [str(k) for k in i]
                     m = int(''.join(str_i), 2)
                     p = self.cptable[m][0]
-                print('%s    %.12f  |  %.12f        ' % (rowName, p, 1-p))
+                print('%s    %.12f  |  %.12f        ' % (rowName, p, 1 - p))
                 print('--------------------------------------------------')
         print('\n')
-        return None
+        return 2 ** len(self.parent)
 
     def __str__(self):
         return self.name
@@ -107,8 +108,16 @@ class Fact:
         self.tail = tail
         self.confidence = confidence
 
+    def factFromTriple(self, triple=('head', 'relation', 'tail')):
+        head = Node(name=triple[0])
+        tail = Node(name=triple[2])
+        rel = Node(name=triple[1])
+        f = Fact(head, rel, tail)
+        return f
+
     def __str__(self):
-        return 'Fact: %s, %s, %s, %.12f' % (self.head.name, self.relation.name, self.tail.name, self.confidence)
+        return 'Fact: %s, %s, %s, %.12f' % \
+               (self.head.name, self.relation.name, self.tail.name, self.confidence)
 
 
 class BN:
@@ -202,23 +211,30 @@ class BN:
         return retVal
 
     def addEdge(self, edge=Edge(Node(), Node())):
-        if (edge.fro, edge.to) not in self.pairsOfEdges().values():  # only one edge between same two nodes
-            if len(edge.fro.child) < 2:  # relation node
+        fro = edge.fro
+        to = edge.to
+        if fro.name in self.namesOfNodes():
+            fro = self.nodeByName(fro.name)
+        if to.name in self.namesOfNodes():
+            to = self.nodeByName(to.name)
+        if (fro, to) not in self.pairsOfEdges().values():  # only one edge between same two nodes
+            if len(fro.child) < 2:  # relation node
                 if edge.label not in self.pairsOfEdges().keys():
-                    self.edges.append(edge)
+                    newedge = Edge(fro, to, label=edge.label)
+                    self.edges.append(newedge)
                     if self.hasCircle():
-                        self.edges.remove(edge)
+                        self.edges.remove(newedge)
                         print('Failed adding edge: There is a circle when adding this edge.')
                     else:
                         # build chain
-                        edge.fro.child.add(edge.to)
-                        edge.to.parent.add(edge.fro)
+                        newedge.fro.child.add(to)
+                        newedge.to.parent.add(fro)
                 else:
                     print('Failed adding edge: Label name "%s" repeated.' % edge)
             else:
                 print('Failed adding edge: A relation node has only two out-degrees.')
         else:
-            print('Failed adding edge: Edge from %s to %s is already existed.' % (edge.fro, edge.to))
+            print('Failed adding edge: Edge from %s to %s is already existed.' % (fro, to))
         return edge
 
     def createBNfromKG(self, file):
@@ -240,9 +256,12 @@ class BN:
                     self.addNode(t)
                 else:
                     t = self.nodeByName(t.name)
-                r = Node(line[1]+str(rcount)); self.addNode(r)
-                rh = Edge(r, h, label=r.name+'2'+h.name); self.addEdge(rh)
-                rt = Edge(r, t, label=r.name+'2'+t.name); self.addEdge(rt)
+                r = Node(line[1] + '~' + str(rcount))
+                self.addNode(r)
+                rh = Edge(r, h, label=r.name + '#2#' + h.name)
+                self.addEdge(rh)
+                rt = Edge(r, t, label=r.name + '#2#' + t.name)
+                self.addEdge(rt)
                 c = line[3]
                 f = Fact(h, r, t, float(c))
                 self.__facts.add(f)
@@ -268,6 +287,14 @@ class BN:
                         self.__facts.add(f)
         return self.__facts
 
+    def findFactByTriple(self, head, relation, tail):
+        for f in self.__facts:
+            if f.head.name == head and \
+                    f.relation.name == relation and \
+                    f.tail.name == tail:
+                return f
+        return None
+
     def sumConfidenceOfFacts(self):
         s = 0
         for f in self.facts():
@@ -286,12 +313,36 @@ class BN:
             ent.add(f.tail)
         return ent
 
+    def namesOfEntities(self):
+        names = set()
+        ents = self.entities()
+        for ent in ents:
+            names.add(ent.name)
+        return names
+
     def relations(self):
         self.facts()
         rel = set()
         for f in self.__facts:
             rel.add(f.relation)
         return rel
+
+    def namesOfRelations(self):
+        names = set()
+        rels = self.relations()
+        for rel in rels:
+            names.add(rel.name)
+        return names
+
+    def typesOfRelations(self):
+        types = set()
+        for name in self.namesOfRelations():
+            loc = name.find('~')
+            if loc != -1:
+                types.add(name[:loc])
+            else:
+                types.add(name)
+        return types
 
     def numberOfEdges(self):
         return len(self.edges)
@@ -347,14 +398,14 @@ class BN:
             table = list()
             p = self.inferEntity(e)
             parents = e.parent
-            for i in range(2**len(parents)):  # 根据1的个数计算概率
+            for i in range(2 ** len(parents)):  # 根据1的个数计算概率
                 binary_row = bin(i)
                 one_number = binary_row.count('1')
                 if one_number == 0:
                     row_p = 1 / (1 + len(parents))
                 else:
                     row_p = p * (one_number / len(parents))
-                row = [row_p, 1-row_p]
+                row = [row_p, 1 - row_p]
                 table.append(np.array(row))
             e.cptable = table
             cptables[e.name] = table
@@ -362,59 +413,177 @@ class BN:
             for f in self.__facts:
                 if f.relation == r:
                     p = self.inferRfromFact(f)
-                    r.cptable = np.array([p, 1-p])
+                    r.cptable = np.array([p, 1 - p])
                     cptables[r.name] = r.cptable
         return cptables
 
-    def inferFact(self, fact = Fact()):
+    def inferFact(self, fact=Fact()):
         # for 't2t' mode, p(fact) = p(r)p(h|r)p(t|r), and r is not in the set of relations
-        confidence = 1E-12
+        confidence = fact.confidence
         r = fact.relation
+        temp_name = r.name
+        if r.name.find('~') != -1:
+            temp_name = r.name[:r.name.find('~')]
         h = fact.head
         t = fact.tail
-        if r in self.relations():
-            confidence = r.cptable[0] * self.condition(h) * self.condition(t)
-        elif h not in self.entities() and t not in self.entities():
-            confidence = np.random.random()
-        elif h in self.entities() and t in self.entities():
-            confidence = 1 - 0  # TODO : computing confidence
-        elif h in self.entities() and t not in self.entities():
-            confidence = 1  # TODO : computing confidence
-            pass
-        elif h not in self.entities() and t in self.entities():
-            pass  # TODO : computing confidence
-        return confidence
+        case = (temp_name in self.typesOfRelations(),  # special handling for relationship
+                h.name in self.namesOfEntities(),
+                t.name in self.namesOfEntities())
+        print('Is (H, R, T) visible = (%s, %s, %s)' % (case[1], case[0], case[2]))
+        if case == (1, 1, 1):  # trivial case
+            for f in self.__facts:
+                if f.head.name == h.name and f.tail.name == t.name and \
+                        f.relation.name[:f.relation.name.find('~')] == r.name:
+                    confidence = f.confidence
+        if case == (1, 0, 0):
+            for rel in self.relations():
+                if rel.name[:rel.name.find('~')] == r.name:
+                    r = rel
+            confidence = r.cptable[0]
+        if case == (1, 0, 1):
+            t = self.nodeByName(t.name)
+            for rel in self.relations():
+                if rel.name[:rel.name.find('~')] != r.name:
+                    continue
+                if t in rel.child:
+                    r = rel
+            confidence = r.cptable[0] * self.condition(t)
+        if case == (1, 1, 0):
+            h = self.nodeByName(h.name)
+            for rel in self.relations():
+                if rel.name[:rel.name.find('~')] != r.name:
+                    continue
+                if h in rel.child:
+                    r = rel
+            confidence = r.cptable[0] * self.condition(h)
+        if case == (0, 0, 0):  # invisible relationship 1 : all r,h,t are invisible
+            # confidence = np.random.random()
+            pe_sum = 0.0
+            for e in self.entities():
+                pe_sum += self.condition(e)
+            ph = pt = pe_sum / (2 * len(self.entities()))
+            pr_sum = 0.0
+            for r in self.relations():
+                pr_sum += r.cptable[0]
+            pr = pr_sum / len(self.relations())
+            confidence = pr * ph * pt
+        if case == (0, 1, 1):  # invisible relationship 2 : only r is invisible
+            h = self.nodeByName(h.name)
+            t = self.nodeByName(t.name)
+            ph = self.condition(h)
+            pt = self.condition(t)
+            pr_sum = list()
+            for r in self.relations():
+                if h in r.child:
+                    pr_sum.append(r.cptable[0])
+                if t in r.child:
+                    pr_sum.append(r.cptable[0])
+            pr = 1 - sum(pr_sum) / len(pr_sum) + math.prod(pr_sum)
+            confidence = pr * ph * pt
+        if case == (0, 1, 0):  # invisible relationship 3 : r and t are invisible
+            h = self.nodeByName(h.name)
+            ph = pt = self.condition(h) / 2
+            pr_sum = list()
+            for r in self.relations():
+                if h in r.child:
+                    pr_sum.append(r.cptable[0])
+            pr = 1 - sum(pr_sum) / (2 * len(pr_sum)) + math.prod(pr_sum)
+            confidence = pr * ph * pt
+        if case == (0, 0, 1):  # invisible relationship 4 : r and h are invisible
+            t = self.nodeByName(t.name)
+            pt = ph = self.condition(t) / 2
+            pr_sum = list()
+            for r in self.relations():
+                if t in r.child:
+                    pr_sum.append(r.cptable[0])
+            pr = 1 - sum(pr_sum) / (2 * len(pr_sum)) + math.prod(pr_sum)
+            confidence = pr * ph * pt
+        fact.confidence = confidence
+        return fact.confidence
 
-    def propagation(self, mode='t2t', node=Node()):
-        pass  # TODO
+    def addFact(self, fact=Fact()):
+        h = fact.head
+        if h.name not in self.namesOfNodes():
+            self.addNode(h)
+        else:
+            h = self.nodeByName(h.name)
+
+        t = fact.tail
+        if t.name not in self.namesOfNodes():
+            self.addNode(t)
+        else:
+            t = self.nodeByName(t.name)
+
+        r = fact.relation
+        r_flag = False
+        if r.name in self.typesOfRelations():
+            for f in self.__facts:
+                if f.head == h and f.tail == t and \
+                        f.relation.name[:f.relation.name.find('~')] == r.name:
+                    r = f.relation
+                    r_flag = True
+                    break
+            if not r_flag:
+                r.name = r.name + '~' + str(len(self.__facts) + 1)
+                self.addNode(r)
+                rh = Edge(r, h, label=r.name + '#2#' + h.name)
+                self.addEdge(rh)
+                rt = Edge(r, t, label=r.name + '#2#' + t.name)
+                self.addEdge(rt)
+        else:
+            r.name = r.name + '~' + str(len(self.__facts) + 1)
+            self.addNode(r)
+            rh = Edge(r, h, label=r.name + '#2#' + h.name)
+            self.addEdge(rh)
+            rt = Edge(r, t, label=r.name + '#2#' + t.name)
+            self.addEdge(rt)
+
+        fact.head = h
+        fact.tail = t
+        fact.relation = r
+        temp = self.findFactByTriple(h.name, r.name, t.name)
+        if temp:
+            fact = temp
+        else:
+            self.__facts.add(fact)
+        self.propagation(mode='t2t')
+        return fact
+
+    def propagation(self, mode='t2t'):
+        # when adding a fact into the knowledge graph, the CPT should be updated
+        self.generateCPT(mode=mode)  # Todo : cost too much !
 
     def __str__(self):
         return '<Class>BN : (%d, %d)' % (len(self.nodes), len(self.edges))
 
 
 if __name__ == "__main__":
-    # Step 1. Build a new Bayesian Network object
+    # Step 1. Build a new Bayesian Network object.
     bayesn = BN()
-    # Step 2. Create a real Bayesian Network from a Knowledge Graph file
+
+    # Step 2. Create a real Bayesian Network from a Knowledge Graph file.
     bayesn.createBNfromKG('../data/bio.csv')
-    # Step 3. Generate Conditional Probabilistic Table for the Bayesian network
+
+    # Step 3. Generate Conditional Probabilistic Table for the Bayesian network.
     bayesn.generateCPT()  # After building the BN, you have to generate a CPT, or else they're random values.
-    # Step 4. Check the Bayesian Network
-    print(bayesn, 'has facts', bayesn.numberOfFacts())
-    for f in bayesn.facts():
-        # f.head.CPT()
-        f.relation.CPT()
-        # f.tail.CPT()
-        pass
-    # Step 5. Perform the approximate inference on the Bayesian Network
 
+    # Step 4. Check the Bayesian Network.
+    print(bayesn, 'has', bayesn.numberOfFacts(), 'facts.')
 
+    # Step 5. Perform the approximate inference on the Bayesian Network.
+    triple = ('cat', 'eat', 'apple')  # a triple denoted by (head, relation, tail)
+    fact = Fact().factFromTriple(triple)  # build a fact object from the triple
+    bayesn.inferFact(fact)  # infer the confidence of the triple as the prior probability of it
 
+    # Step 6. After inferring, the invisible fact should be added into the KB!
+    fact = bayesn.addFact(fact)  # adding a fact and updating cptable of its nodes; must reassign the fact!
 
+    # Checking whether everything is correct or not?
+    print(bayesn, 'has', bayesn.numberOfFacts(), 'facts.')
+    print(triple, bayesn.inferFact(fact))
+    fact.head.CPT()
+    fact.tail.CPT()
+    fact.relation.CPT()
 
-
-
-
-
-
+    # Step 7. Inferring invisible facts based on the Bayesian network.
 
